26a839870fcc36f27d17310ff653b86e04d16d41
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-02-25 00:31:53 +0300

    Update crossfi.sh

--- crossfi.sh
+++ crossfi.sh
@@ -1,1 +1,147 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://raw.githubusercontent.com/CoinHuntersTR/Andromeda-Testnet-Rehberi/main/logo.sh | bash && sleep 1
 
+read -p "Cüzdan İsmi Giriniz:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Validator İsmi Giriniz :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "PORT Girin, Değişiklik yapmayacaksanız 26 yazın! :" PORT
+echo 'export PORT='$PORT
+
+# set vars
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export CROSSFI_CHAIN_ID="crossfi-evm-testnet-1"" >> $HOME/.bash_profile
+echo "export CROSSFI_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$CROSSFI_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$CROSSFI_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.21.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+cd $HOME
+wget https://github.com/crossfichain/crossfi-node/releases/download/v0.3.0-prebuild3/crossfi-node_0.3.0-prebuild3_linux_amd64.tar.gz && tar -xf crossfi-node_0.3.0-prebuild3_linux_amd64.tar.gz
+tar -xvf crossfi-node_0.3.0-prebuild3_linux_amd64.tar.gz
+chmod +x $HOME/bin/crossfid
+mv $HOME/bin/crossfid $HOME/go/bin
+rm -rf crossfi-node_0.3.0-prebuild3_linux_amd64.tar.gz $HOME/bin
+
+printGreen "5. Configuring and init app..." && sleep 1
+# config and init app
+crossfid config node tcp://localhost:${CROSSFI_PORT}657
+crossfid config keyring-backend os
+crossfid config chain-id crossfi-evm-testnet-1
+rm -rf testnet ~/.mineplex-chain
+git clone https://github.com/crossfichain/testnet.git
+mv $HOME/testnet/ $HOME/.mineplex-chain/
+sleep 1
+echo done
+
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+wget -O $HOME/.mineplex-chain/config/genesis.json https://testnet-files.itrocket.net/crossfi/genesis.json
+wget -O $HOME/.mineplex-chain/config/addrbook.json https://testnet-files.itrocket.net/crossfi/addrbook.json
+sleep 1
+echo done
+
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+SEEDS="dd83e3c7c4e783f8a46dbb010ec8853135d29df0@crossfi-testnet-seed.itrocket.net:36656"
+PEERS="66bdf53ec0c2ceeefd9a4c29d7f7926e136f114a@crossfi-testnet-peer.itrocket.net:36656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.mineplex-chain/config/config.toml
+
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${CROSSFI_PORT}317%g;
+s%:8080%:${CROSSFI_PORT}080%g;
+s%:9090%:${CROSSFI_PORT}090%g;
+s%:9091%:${CROSSFI_PORT}091%g;
+s%:8545%:${CROSSFI_PORT}545%g;
+s%:8546%:${CROSSFI_PORT}546%g;
+s%:6065%:${CROSSFI_PORT}065%g" $HOME/.mineplex-chain/config/app.toml
+
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${CROSSFI_PORT}658%g;
+s%:26657%:${CROSSFI_PORT}657%g;
+s%:6060%:${CROSSFI_PORT}060%g;
+s%:26656%:${CROSSFI_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${CROSSFI_PORT}656\"%;
+s%:26660%:${CROSSFI_PORT}660%g" $HOME/.mineplex-chain/config/config.toml
+
+# config pruning
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.mineplex-chain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.mineplex-chain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.mineplex-chain/config/app.toml
+
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "10000000000000mpx"|g' $HOME/.mineplex-chain/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.mineplex-chain/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.mineplex-chain/config/config.toml
+sleep 1
+echo done
+
+# create service file
+sudo tee /etc/systemd/system/crossfid.service > /dev/null <<EOF
+[Unit]
+Description=crossfi node
+After=network-online.target
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.mineplex-chain
+ExecStart=$(which crossfid) start --home $HOME/.mineplex-chain
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+# reset and download snapshot
+crossfid tendermint unsafe-reset-all --home $HOME/.mineplex-chain
+if curl -s --head curl https://testnet-files.itrocket.net/crossfi/snap_crossfi.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://testnet-files.itrocket.net/crossfi/snap_crossfi.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.mineplex-chain
+    else
+  echo no have snap
+fi
+
+# enable and start service
+sudo systemctl daemon-reload
+sudo systemctl enable crossfid
+sudo systemctl restart crossfid && sudo journalctl -u crossfid -f
